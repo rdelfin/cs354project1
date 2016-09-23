03f68efa876587fae30e0d90e022ed6697288cb4
Ricardo Delfin Garcia <ricardo.delfin.garcia@gmail.com>
2016-09-23 13:52:55 -0500

    First fix, upper bound to beta and gamma

--- src/SceneObjects/trimesh.cpp
+++ src/SceneObjects/trimesh.cpp
@@ -100,40 +100,34 @@ bool TrimeshFace::intersectLocal(ray& r, isect& i) const
         return false;
 
     // For our ray, p(t) = (P + td), we can solve for t and get: t = -(n*P * d)/(n*d)
-    double t = -(glm::dot(normal, r.p) - dist)/(glm::dot(normal, r.d));
+    double t = (dist - glm::dot(normal, r.p))/(glm::dot(normal, r.d));
 
     glm::dvec3 p = r.p + t*r.d; // Value of p(i.t)
 
-    glm::dvec3 uVec = b - a;
-    glm::dvec3 vVec = c - a;
-
-    // Cramer's rule solution set:
-    glm::dmat3x3 denominator = {{a.x,b.x,c.x},{a.y,b.y,c.y},{a.z,b.z,c.z}};
-    glm::dmat3x3 alphaNumerator = denominator;
-    glm::dmat3x3 betaNumerator = denominator;
-    glm::dmat3x3 gammaNumerator = denominator;
-
-    alphaNumerator[0][0] = betaNumerator[0][1] = gammaNumerator[0][2] = p.x;
-    alphaNumerator[1][0] = betaNumerator[1][1] = gammaNumerator[1][2] = p.y;
-    alphaNumerator[2][0] = betaNumerator[2][1] = gammaNumerator[2][2] = p.z;
-
-    double denDet = glm::determinant(denominator);
+    double denDet = glm::dot(a, glm::cross(b, c));
 
 
-    double w = glm::determinant(alphaNumerator)/denDet;
-    double u = glm::determinant(betaNumerator)/denDet;
-    double v = glm::determinant(gammaNumerator)/denDet;
+    double alpha = glm::dot(p, glm::cross(b, c))/denDet;
+    double beta = glm::dot(a, glm::cross(p, c))/denDet;
+    double gamma = glm::dot(a, glm::cross(b, p))/denDet;
 
-    bool intersects = t >=0 && u >= 0 && u <= 1 && v >=0 && v <= 1;
+    bool intersects = /*t >=0 &&*/ beta >= 0 && gamma >=0 && (beta + gamma) <= 1;
+
+    if(intersects) {
+        std::cout << "INTERSECTS" << std::endl;
+        std::cout << "TRIANGLE: (" << a.x << ", " << a.y << ", " << a.z << "), (" << b.x << ", " << b.y << ", " << b.z << "), (" << c.x << ", " << c.y << ", " << c.z << "), " << std::endl;
+        std::cout << "RAY: P(t) = (" << r.p.x << ", " << r.p.y << ", " << r.p.z << ")" << " + (" << r.d.x << ", " << r.d.y << ", " << r.d.z << ")t" << std::endl;
+        std::cout << "t: " << t << std::endl;
+        std::cout << std::endl;
+    }
 
     //std::cout << "T: " << t << ", alpha: " << alpha << ", beta: " << beta << std::endl;
     //std::cout << "r.P: (" << r.p.x << ", " << r.p.y << ", " << r.p.z << ")" << "r.d: (" << r.d.x << ", " << r.d.y << ", " << r.d.z << ")" << std::endl;
     //std::cout << "a: (" << a.x << ", " << a.y << ", " << a.z << "), b: (" << b.x << ", " << b.y << ", " << b.z << "), c: (" << c.x << ", " << c.y << ", " << c.z << ")" << ", n: (" << normal.x << ", " << normal.y << ", " << normal.z << ")" << std::endl;
 
     if(intersects) {
+        i.setBary(alpha, beta, gamma);
         i.t = t;
-        i.uvCoordinates.x = w;
-        i.uvCoordinates.y = u;
         i.material = new Material(*material);
     }
 
